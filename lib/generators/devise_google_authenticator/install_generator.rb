dc2fc24ee009280c6298d5ef024946928b387376
Said Kaldybaev <said.kaldybaev@every-pay.com>
2015-05-15 12:17:59 +0300

    tweaking generator file

--- lib/generators/devise_google_authenticator/install_generator.rb
+++ lib/generators/devise_google_authenticator/install_generator.rb
@@ -8,6 +8,8 @@ module DeviseGoogleAuthenticator
 
       def add_configs
         inject_into_file "config/initializers/devise.rb", "\n  # ==> Devise Google Authenticator Extension\n  # Configure extension for devise\n\n" +
+        "  # Maximum login attempts:\n" +
+        "  # config.ga_max_login_attempts = 3\n\n" +
         "  # How long should the user have to enter their token. To change the default, uncomment and change the below:\n" +
         "  # config.ga_timeout = 3.minutes\n\n" +
         "  # Change time drift settings for valid token values. To change the default, uncomment and change the below:\n" +
@@ -20,7 +22,11 @@ module DeviseGoogleAuthenticator
         "  # config.ga_appname = 'example.com'\n\n" +
         "  # Change setting to bypass the Display QR page immediately after a user sign's up\n" +
         "  # To change the default, uncomment and change the below. Defaults to false:\n" + 
-        "  # config.ga_bypass_signup = true\n\n" + 
+        "  # config.ga_bypass_signup = true\n\n" +
+        "  # The length of backup codes:\n" +
+        "  # config.ga_backup_code_length = 16\n\n" +
+        "  # Number of backup codes:\n" +
+        "  # config.ga_number_of_backup_codes = 5\n\n" +
         "\n", :before => /end[ |\n|]+\Z/
       end
 
